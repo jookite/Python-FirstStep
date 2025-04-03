130fe006e58d12eb20e20a1c0cac45f9668076a6
Seo jooyeon <seojookite@gmail.com>
2025-04-03 14:59:31 +0900

    Create asgmt04.py
    feat;add week04/asgmt04

---
+++ jookite/CP1/week04/asgmt04.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+result = ""
+
+if n == 0:
+    print("0")
+
+while n!=0:
+    if n%2 == 0:
+        result = "0"+result
+    else:
+        result = "1"+result
+    n >>= 1
+
+print(result)
